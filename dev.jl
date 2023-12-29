da4e08d717bb33981af5bce834f6377444b05423
Umut <64823238+BayPatates@users.noreply.github.com>
2023-12-29 21:26:36 +0300

    Birçok Değişiklik ve Hata Düzeltmesi
    40. ve 41. sayfadan örnekler aldırttım çünkü diziler tarihsel olarak sıralanmış ve yeni diziler daha bitmediği için hata alıyoruz. (değerler çok orantısız oluyor.)
    "for" döngülerini sadeleştirdim ve bu sayede program daha verimli oldu.
    Julia programlama dili MatLab gibi dillerden farklı olarak özellikler (features) satırlara ve örnekler (samples) sütunlara yazılmalıdır. (Yanlış biliyorsam düzeltin ama diğer türlü zaten çalışmıyor, hata veriyor.)
    PCA'de 40. ve 41. sayfaları aldığımızdan daha fazla doğruluk payı elde ettik. Yani 2 tane boyuta indirgeyince bile yüksek doğruluk elde ediyoruz.
    Bunun dışında bazı küçük değişiklikler de yaptım.
    
    Not: Julia dilinde satır ve sütunun nasıl işlediğini unutmayalım. Çoğu programlama dilinden farklı çalışıyor. Bu durum fonksiyonları da etkiliyor.

--- dev.jl
+++ dev.jl
@@ -1,5 +1,4 @@
 using HTTP, JSON
-#using Plots
 
 const API_KEY = "08333c23ecfae8c3874ac3fd49b41e67"
 
@@ -55,13 +54,12 @@ end
 
 
 all_tv_show_ids = []
-for page in 2:2
+for page in 40:41
     tv_show_ids = fetch_tv_show_ids(page)
     append!(all_tv_show_ids, tv_show_ids)
 end
-sayi = length(all_tv_show_ids)
 series = TvSeries[]
-for x in 1:sayi
+for x in eachindex(all_tv_show_ids)
     push!(series, fetch_tv_series(all_tv_show_ids[x]))
 end
 
@@ -73,47 +71,38 @@ end
 
 
 
-
-
-
 # PCA Test
 using MultivariateStats
 using StatsBase
 
 # Veri setini hazırla (sayısal özellikleri kullan)
+# Ayrıca Julia'da öncelik sütun'da olmalı. Diğer dillerden farklı olarak bu dilde satırda özellikler ve sütunda örnekler olmalı.
+# Bu yüzden bu matrixin transpose'unu alıyoruz.
+# Eğer yanlış biliyorsam söyleyin düzeltelim ama böyle program normal çalışıyor.
 numeric_features = hcat([series[i].episode_run_time for i in eachindex(series)],
     [series[i].number_of_episodes for i in eachindex(series)],
     [series[i].number_of_seasons for i in eachindex(series)],
-    [series[i].popularity for i in eachindex(series)])
+    [series[i].popularity for i in eachindex(series)])'
 
 # Veriyi normalize et
 #https://youtu.be/ZWyoSZk-Uq0?t=500
 # row = feature, column = sample
-normalized_features = zscore(numeric_features, 1)
-normalized_features = normalized_features'
-println("normalized_features:")
-display("text/plain", normalized_features)
-
+normalized_features = zscore(numeric_features)
 # PCA modelini oluştur
-pca_model = fit(PCA, normalized_features; maxoutdim=3)
-# 4 tane input  dimension'dan:
-# 1 tane output dimension bilginin %43.25'ini tutuyor.
-# 2 tane output dimension bilginin %70.17'sini tutuyor.
-# 3 tane output dimension bilginin %92.24'ünü tutuyor.
-# Yani 3 tane output olması iyi sonuç veriyor. (4 tanesi zaten PCA değil, direkt inputun kendisi)
-
+pca_model = fit(PCA, normalized_features; maxoutdim=2)
 
 # Boyut azaltılmış veriyi al
 reduced_features = transform(pca_model, normalized_features)
 
 # Sonucu göster
 println("\nBoyut Azaltılmış Veri Seti:")
-println(length(reduced_features), " tane indexsi ve $(size(reduced_features, 2)) tane sütunu var. reduced_features[] array'i (3x20)'lik bir matrix yani loop 20 kere tekrarlanıcak.")
+#println(length(reduced_features), " tane indexsi ve $(size(reduced_features, 2)) tane sütunu var. reduced_features[] array'i (3x20)'lik bir matrix yani loop 20 kere tekrarlanıcak.")
 # axes(reduced_features, 2) fonksiyonu sütun sayısını döndürür. (1=satır, 2=sütun, 3...=diğer boyutlar) (axes = eksenler (x,y,z gibi))
 for i in axes(reduced_features, 2)
     println("TV Serisi $(i): ", reduced_features[:, i])
 end
 
+#=
 println("\nreduced_features matrix'i böyle:")
 display("text/plain", reduced_features)
 
@@ -123,10 +112,12 @@ println("\nreversed_reduced_features matrix'i böyle:")
 display("text/plain", reversed_reduced_features)
 println("\nAsıl (PCA uygulanmamış) matrix böyleydi:")
 display("text/plain", normalized_features)
-#Sonuç
-println("\n Buradan anlıyoruz ki veriyi eski haline dönüştürmeye çalıştırdığımızda veri %92.24215076926459 oranla orjinaline benziyor. Yani verinin doğruluğu konusunda %7.757849230735414 gibi bir kayıp söz konusu oluyor.")
+=#
 
-#plotlyjs(size = (360, 360))
+
+
+
+#K-Means Test
 using Clustering
 using Plots
 
@@ -134,30 +125,28 @@ using Plots
 k = 3
 
 #Model oluştur
-kmeans_model = kmeans(reduced_features', k)
+kmeans_model = kmeans(reduced_features, k)
 
 #Kümeleri tahmin etme
 cluster_assignments = assignments(kmeans_model)
 
 #Küme merkezlerini bulma
-cluster_centers = kmeans_model.centers'
+cluster_centers = kmeans_model.centers
 
 #Sonuçları print etme
 println("\nK-Means Kümeleme Sonuçları:")
-for i in 1:k
+for i in eachindex(cluster_assignments)
     println("TV Serisi $i küme: ", cluster_assignments[i])
 end
 
 #Merkezleri print etme
 println("\nKüme Merkezleri:")
-for i in 1:k
+for i in axes(cluster_centers, 2)
     println("Küme $i Merkezi: ", cluster_centers[:, i])
 end
 
 #Plot olarak gösterir
-scatter(reduced_features[1, :], reduced_features[2, :], reduced_features[3, :], color=cluster_assignments, marker=:auto, xlabel="Principal Component 1", ylabel="Principal Component 2", zlabel="Principal Component 3", legend=false)
-scatter!(cluster_centers[1, :], cluster_centers[2, :], cluster_centers[3, :], color=:red, markersize=8, label="Cluster Centers")
-
-
-
+scatter(reduced_features[1, :], reduced_features[2, :], color=cluster_assignments, marker=:auto, xlabel="Principal Component 1", ylabel="Principal Component 2", zlabel="Principal Component 3", legend=false)
+scatter!(cluster_centers[1, :], cluster_centers[2, :], color=:red, markersize=8, label="Cluster Centers")
+# Bazı verilerde hata var, bu yüzden çok dağınık görülüyorlar. Bizim bu hatalı verileri ayıklamamız gerek.
 
